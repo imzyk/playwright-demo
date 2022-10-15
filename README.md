edd835deea48639d17c6594dbeb447d99505938d
peter.zhang <peter.zhang@binance.com>
2022-10-15 16:32:47 +1300

    Update readme and other tiny stuff

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Highights
 * Using of JavaScript variation of Playwright
 * Using of POM
+* Demo of both BDD(branch of add-bdd) and traditional way (master branch)
--- tests/shop.spec.js
+++ tests/shop.spec.js
@@ -8,34 +8,32 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
-test.describe("Positive Scenario", () => {
-  test('Shop products', async ({ page }) => {
-    const menuPage = new MenuPage(page);
-    await menuPage.browseToShop();
-    const shopPage = new ShopPage(page);
-    const expectedProducts = {
-      "Stuffed Frog": 2,
-      "Fluffy Bunny": 5,
-      "Valentine Bear": 3
-    };
-    let expectedProductsPrices = {}
-    for (let key of Object.keys(expectedProducts)) {
-      console.log(`Purchase ${key}`);
-      await shopPage.buyProduct(key, expectedProducts[key]);
-      //record price for later validation
-      expectedProductsPrices[key] = await shopPage.getPrice(key);
-    }
+test('Shop products', async ({ page }) => {
+  const menuPage = new MenuPage(page);
+  await menuPage.browseToShop();
+  const shopPage = new ShopPage(page);
+  const expectedProducts = {
+    "Stuffed Frog": 2,
+    "Fluffy Bunny": 5,
+    "Valentine Bear": 3
+  };
+  let expectedProductsPrices = {}
+  for (let key of Object.keys(expectedProducts)) {
+    console.log(`Purchase ${key}`);
+    await shopPage.buyProduct(key, expectedProducts[key]);
+    //record price for later validation
+    expectedProductsPrices[key] = await shopPage.getPrice(key);
+  }
 
-    await menuPage.browseToCart();
-    const cartPage = new CartPage(page);
-    let totalValue = 0;
-    for (let item of await cartPage.getItems()) {
-      console.log(`Parse ${JSON.stringify(item)}`);
-      expect(Number(item["price"])).toBe(Number(expectedProductsPrices[item["name"]]));
-      let subTotal = expectedProductsPrices[item["name"]] * expectedProducts[item["name"]];
-      totalValue += subTotal;
-      expect(Number(item["subTotal"])).toBe(Number(subTotal));
-    }
-    expect(Number(await cartPage.getTotal())).toBe(Number(totalValue));
-  });
+  await menuPage.browseToCart();
+  const cartPage = new CartPage(page);
+  let totalValue = 0;
+  for (let item of await cartPage.getItems()) {
+    console.log(`Parse ${JSON.stringify(item)}`);
+    expect(Number(item["price"])).toBe(Number(expectedProductsPrices[item["name"]]));
+    let subTotal = expectedProductsPrices[item["name"]] * expectedProducts[item["name"]];
+    totalValue += subTotal;
+    expect(Number(item["subTotal"])).toBe(Number(subTotal));
+  }
+  expect(Number(await cartPage.getTotal())).toBe(Number(totalValue));
 });
